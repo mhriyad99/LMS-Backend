75a2cf25a7f9a9e35307abe65ee9617dcd436f91
mhriyad99 <mhriyad98@gmail.com>
2026-05-15 23:20:58 +0600

    May 15: alembic initialization

--- app/config/database.py
+++ app/config/database.py
@@ -1,8 +1,10 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from app.config.settings import settings
 
-DATABASE_URL = "postgresql+asyncpg://postgres:password@localhost:5432/lms"
+DATABASE_URL = (f"postgresql+asyncpg://postgres:{settings.DB_PASSWORD}"
+                f"@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}")
 engine = create_async_engine(DATABASE_URL)
 AsyncSessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
 Base = declarative_base()
--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from app.routers import books, book_copy, users, auth
 
 app = FastAPI()
 
-# models.Base.metadata.create_all(bind=engine)
-
 app.include_router(books.router)
 app.include_router(book_copy.router)
 app.include_router(users.router)
